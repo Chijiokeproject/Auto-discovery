7120ecf7c017b210b9580ee11ad58570339154d1
Chijiokeproject <alaukwu.chijioke@cloudhight.com>
2025-05-25 00:21:42 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region  = "eu-west-2"
-  profile = "auto-discovery"
 }
 
 provider "vault" {
@@ -14,6 +13,5 @@ terraform {
     key          = "infrastructure/terraform.tfstate"
     region       = "eu-west-2"
     use_lockfile = true
-    profile      = "auto-discovery"
   }
 }
