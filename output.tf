62f284e304dfe815cc2a5dd189952b7cc5327583
Chijiokegit <chijiokedevops@yahoo.com>
2025-06-17 00:32:24 +0100

    coded5

--- output.tf
+++ output.tf
@@ -5,9 +5,8 @@ output "bastion-public-ip" {
   value = module.bastion.bastion_public_ip
 }
 output "nexus_ip" {
-  value = aws_instance.nexus.private_ip
+  value = module.nexus.nexus_ip
 }
-
 output "nexus_public_ip" {
   value = module.nexus.nexus_public_ip
 }
