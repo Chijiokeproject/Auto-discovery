e70937d5b01563ddb79ee6c0021e1f5106f2f1b1
Chijiokegit <chijiokedevops@yahoo.com>
2025-06-18 01:13:44 +0100

    deploymeny code00

--- module/prod-envi/variable.tf
+++ module/prod-envi/variable.tf
@@ -2,7 +2,7 @@ variable "name" {}
 variable "vpc-id" {}
 variable "bastion_sg" {}
 variable "key-name" {}
-variable "pri-subnet1" {}
+variable "pri_subnet1" {}
 variable "pri_subnet2" {}
 variable "pub_subnet1" {}
 variable "pub_subnet2" {}
