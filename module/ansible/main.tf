2997b12ab053dcb520169aef73251be4e8e93731
Chijiokegit <chijiokedevops@yahoo.com>
2025-06-19 21:23:58 +0100

    real1

--- module/ansible/main.tf
+++ module/ansible/main.tf
@@ -93,15 +93,23 @@ resource "aws_iam_role_policy_attachment" "s3-policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
+#resource "null_resource" "ansible-setup" {
+ # provisioner "local-exec" {
+  #  command = <<EOT
+   #   /usr/local/bin/aws s3 cp --recursive ${path.module}/script/ s3://chijioke-bucket-auto-discovery-1/ansible-script/
+   # EOT
+   # interpreter = ["/bin/bash", "-c"]
+  #}
+#}
+
 resource "null_resource" "ansible-setup" {
   provisioner "local-exec" {
     command = <<EOT
-      /usr/local/bin/aws s3 cp --recursive ${path.module}/script/ s3://chijioke-bucket-auto-discovery-1/ansible-script/
+      $(which aws) s3 cp --recursive ${path.module}/script/ s3://chijioke-bucket-auto-discovery-1/ansible-script/
     EOT
     interpreter = ["/bin/bash", "-c"]
   }
 }
-
 
 
 #resource "null_resource" "ansible-setup" {
