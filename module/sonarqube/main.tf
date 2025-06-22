e4180f9fe7f0228e68eb60556b41cf1fbbed896f
Chijiokegit <chijiokedevops@yahoo.com>
2025-06-22 21:03:29 +0100

    great

--- module/sonarqube/main.tf
+++ module/sonarqube/main.tf
@@ -107,7 +107,7 @@ resource "aws_elb" "sonarqube_elb" {
     instance_protocol  = "http"
     lb_port            = 443
     lb_protocol        = "https"
-    ssl_certificate_id = var.acm_certificate_arn
+    ssl_certificate_id = var.auto_acm_cert.arn
   }
 
   health_check {
